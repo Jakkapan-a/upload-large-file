9b6c3b0c48e6242344974a5927c25c0d3dd55c7f
Jakkapan1997 <67558529+Jakkapan1997@users.noreply.github.com>
2022-12-02 22:27:38 +0700

    install package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import '../sass/app.scss';
+
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import * as resumablejs from "resumablejs";
+window.Resumable = resumablejs;
